881d53e5d88e7a548d19deadb52675966bfe8369
jeanesterhuyse <jeanesterhuyse01@gmail.com>
2021-09-29 11:50:59 +0200

    Started with database and did swagger

--- ASDomain/src/main/java/JeanEstProject/domain/exception/AccountTransaction.java
+++ ASDomain/src/main/java/JeanEstProject/domain/exception/AccountTransaction.java
@@ -1,4 +1,77 @@
 package JeanEstProject.domain.exception;
 
-public class AccountTransaction {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+@Entity
+@Table(name = "ACCOUNT_TX", schema = "ACCOUNTSYSTEM")
+public class AccountTransaction implements Serializable {
+
+    private static final long serialVersionUID = -8178985468958706058L;
+    private Long transactionId;
+    private AccountType accountType;
+    private Long memberId;
+    private Long amount;
+    private LocalDate transactionDate;
+    public AccountTransaction() {
+    }
+
+    public AccountTransaction(Long transactionId, AccountType accountType, Long memberId, Long amount, LocalDate transactionDate) {
+        this.transactionId = transactionId;
+        this.accountType = accountType;
+        this.memberId = memberId;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+    }
+    @Id
+    @SequenceGenerator(name = "GENERIC_SEQ", sequenceName = "ACCOUNTSYSTEM.GENERIC_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "VIT_RSA_GENERIC_SEQ")
+
+    @Column(name = "TX_ID")
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Column(name = "TYPE_ID")
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountTypeId(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    @Column(name = "TX_ID")
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    @Column(name = "AMOUNT")
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    @Column(name = "TX_DATE")
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+@ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name = "TYPE_ID")
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
 }
+
+
--- ASDomain/src/main/java/JeanEstProject/domain/exception/AccountType.java
+++ ASDomain/src/main/java/JeanEstProject/domain/exception/AccountType.java
@@ -1,4 +1,94 @@
 package JeanEstProject.domain.exception;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Set;
 
-public class AccountType {
+@Entity
+@Table(name= "ACCOUNTTYPE",schema= "ACCOUNTSYSTEM")
+public class AccountType implements Serializable {
+    @Id
+    @SequenceGenerator(name= "DiscoverySequence", sequenceName= "ACCOUNTSYSTEM.DiscoverySequence", allocationSize=1)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator= "DiscoverySequence")
+
+    private String AT_ID;
+    private String memonic;
+    private String accountTypeName;
+    private LocalDate dateCreated;
+    private Set<AccountTransaction>accountTransaction;
+    public AccountType(String AT_ID, String memonic, String accountTypeName, LocalDate dateCreated) {
+        this.AT_ID = AT_ID;
+        this.memonic = memonic;
+        this.accountTypeName = accountTypeName;
+        this.dateCreated = dateCreated;
+    }
+
+    public AccountType() {
+    }
+    @Column(name= "AT_ID")
+    public String getAT_ID() {
+        return AT_ID;
+    }
+
+    public void setAT_ID(String AT_ID) {
+        this.AT_ID = AT_ID;
+    }
+    @Column(name= "MEMONIC")
+    public String getMemonic() {
+        return memonic;
+    }
+
+    public void setMemonic(String memonic) {
+        this.memonic = memonic;
+    }
+    @Column(name= "TYPE_NAME")
+    public String getAccountTypeName() {
+        return accountTypeName;
+    }
+
+    public void setAccountTypeName(String accountTypeName) {
+        this.accountTypeName = accountTypeName;
+    }
+    @Column(name= "DATE_CREATED")
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    @OneToMany(targetEntity = AccountTransaction.class, fetch = FetchType.LAZY, mappedBy = "accountType", orphanRemoval = true, cascade = CascadeType.PERSIST)
+    public Set<AccountTransaction> getAccountTransaction(){
+        return accountTransaction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountType that = (AccountType) o;
+        return Objects.equals(AT_ID, that.AT_ID) &&
+                Objects.equals(memonic, that.memonic) &&
+                Objects.equals(accountTypeName, that.accountTypeName) &&
+                Objects.equals(dateCreated, that.dateCreated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(AT_ID, memonic, accountTypeName, dateCreated);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountType{" +
+                "AT_ID='" + AT_ID + '\'' +
+                ", memonic='" + memonic + '\'' +
+                ", accountTypeName='" + accountTypeName + '\'' +
+                ", dateCreated=" + dateCreated +
+                '}';
+    }
 }
